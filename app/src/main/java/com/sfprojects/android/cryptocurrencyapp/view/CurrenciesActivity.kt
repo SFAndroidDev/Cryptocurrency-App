9f1cd46c0af2213158b85c2d2683f1bf53eecfbe
SF <sfnaiech2016@gmail.com>
2020-10-26 09:41:36 +0100

    adding the viewModel for the currenciesActivity

--- app/src/main/java/com/sfprojects/android/cryptocurrencyapp/view/CurrenciesActivity.kt
+++ app/src/main/java/com/sfprojects/android/cryptocurrencyapp/view/CurrenciesActivity.kt
@@ -2,10 +2,12 @@ package com.sfprojects.android.cryptocurrencyapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sfprojects.android.cryptocurrencyapp.R
 
 class CurrenciesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_currencies)
+
     }
 }
---
+++ app/src/main/java/com/sfprojects/android/cryptocurrencyapp/viewmodel/CurrenciesActivityViewModel.kt
@@ -0,0 +1,11 @@
+package com.sfprojects.android.cryptocurrencyapp.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+/**
+ * Created by SFDevelop on 10/26/20.
+ */
+class CurrenciesActivityViewModel(application: Application): AndroidViewModel(application) {
+
+}
