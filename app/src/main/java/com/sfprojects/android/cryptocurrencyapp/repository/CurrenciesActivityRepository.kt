8ea3428deb312483774e6db1e24dfeff265c2536
SF <sfnaiech2016@gmail.com>
2020-10-26 09:44:37 +0100

    adding the repository for the currenciesActivity

---
+++ app/src/main/java/com/sfprojects/android/cryptocurrencyapp/repository/CurrenciesActivityRepository.kt
@@ -0,0 +1,10 @@
+package com.sfprojects.android.cryptocurrencyapp.repository
+
+import android.app.Application
+
+/**
+ * Created by SFDevelop on 10/26/20.
+ */
+class CurrenciesActivityRepository(application: Application) {
+
+}
