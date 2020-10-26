5e2dadfd2b795dd204ccc5a01dd7f739b113e351
SF <sfnaiech2016@gmail.com>
2020-10-26 14:43:36 +0100

    getting the ViewModel in the CurrnciesActivity

--- app/src/main/java/com/sfprojects/android/cryptocurrencyapp/view/CurrenciesActivity.kt
+++ app/src/main/java/com/sfprojects/android/cryptocurrencyapp/view/CurrenciesActivity.kt
@@ -2,12 +2,21 @@ package com.sfprojects.android.cryptocurrencyapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.sfprojects.android.cryptocurrencyapp.R
+import com.sfprojects.android.cryptocurrencyapp.viewmodel.CurrenciesActivityViewModel
 
 class CurrenciesActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: CurrenciesActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_currencies)
+
+        viewModel = ViewModelProvider(this)
+            .get(CurrenciesActivityViewModel::class.java)
+
 
     }
 }
