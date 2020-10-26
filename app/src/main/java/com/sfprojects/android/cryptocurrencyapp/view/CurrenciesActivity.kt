292e71a799a676ad0f5c38358df6cf29566d5b07
SF <sfnaiech2016@gmail.com>
2020-10-26 09:31:31 +0100

    adding the currencies list activity

---
+++ app/src/main/java/com/sfprojects/android/cryptocurrencyapp/view/CurrenciesActivity.kt
@@ -0,0 +1,11 @@
+package com.sfprojects.android.cryptocurrencyapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CurrenciesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_currencies)
+    }
+}
