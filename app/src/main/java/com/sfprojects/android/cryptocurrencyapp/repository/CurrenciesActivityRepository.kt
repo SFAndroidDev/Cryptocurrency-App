b954c50612cb23093dec1bd0c4d8944069209d22
SF <sfnaiech2016@gmail.com>
2020-10-26 14:51:25 +0100

    setting the repository and the live data in the view model

--- app/src/main/java/com/sfprojects/android/cryptocurrencyapp/repository/CurrenciesActivityRepository.kt
+++ app/src/main/java/com/sfprojects/android/cryptocurrencyapp/repository/CurrenciesActivityRepository.kt
@@ -1,10 +1,12 @@
 package com.sfprojects.android.cryptocurrencyapp.repository
 
 import android.app.Application
+import androidx.lifecycle.MutableLiveData
 
 /**
  * Created by SFDevelop on 10/26/20.
  */
 class CurrenciesActivityRepository(application: Application) {
 
+    val showProgress: MutableLiveData<Boolean>()
 }
--- app/src/main/java/com/sfprojects/android/cryptocurrencyapp/viewmodel/CurrenciesActivityViewModel.kt
+++ app/src/main/java/com/sfprojects/android/cryptocurrencyapp/viewmodel/CurrenciesActivityViewModel.kt
@@ -2,10 +2,18 @@ package com.sfprojects.android.cryptocurrencyapp.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.sfprojects.android.cryptocurrencyapp.repository.CurrenciesActivityRepository
 
 /**
  * Created by SFDevelop on 10/26/20.
  */
 class CurrenciesActivityViewModel(application: Application): AndroidViewModel(application) {
 
+    val repository = CurrenciesActivityRepository(application)
+    val showProgress: LiveData<Boolean>
+
+    init{
+        this.showProgress = repository.showProgress
+    }
 }
