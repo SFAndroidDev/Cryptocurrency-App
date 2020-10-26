adac569985eaf0f1251abce9f64e914d0e35cb21
SF <sfnaiech2016@gmail.com>
2020-10-26 15:58:35 +0100

    adding retrofit2 and GSON dependencies

--- app/src/main/java/com/sfprojects/android/cryptocurrencyapp/viewmodel/DetailsActivityViewModel.kt
+++ app/src/main/java/com/sfprojects/android/cryptocurrencyapp/viewmodel/DetailsActivityViewModel.kt
@@ -2,10 +2,12 @@ package com.sfprojects.android.cryptocurrencyapp.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.sfprojects.android.cryptocurrencyapp.repository.DetailsActivityRepository
 
 /**
  * Created by SFDevelop on 10/26/20.
  */
 class DetailsActivityViewModel(application: Application) : AndroidViewModel(application){
 
+    private val repository = DetailsActivityRepository(application)
 }
