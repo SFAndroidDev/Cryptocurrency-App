7660d953cf99f81caa890993ddb952115b2524cd
SF <sfnaiech2016@gmail.com>
2020-10-29 18:08:42 +0100

    correcting the api request

--- app/src/main/java/com/sfprojects/android/cryptocurrencyapp/network/CurrenciesNetwork.kt
+++ app/src/main/java/com/sfprojects/android/cryptocurrencyapp/network/CurrenciesNetwork.kt
@@ -1,16 +1,17 @@
 package com.sfprojects.android.cryptocurrencyapp.network
 
+import com.sfprojects.android.cryptocurrencyapp.network.model.CoinResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 /**
  * Created by SFDevelop on 10/26/20.
  */
 const val BASE_URL = "https://min-api.cryptocompare.com/data/top/"
-const val API_KEY = "a0aa29cbb5149fbc4dd1e5a390412fc8fd997d9110802c869a65101be40e0aa7"
 
 interface CurrenciesNetwork {
 
-    @GET("mktcapfull?tsym=EUR&page=20&api_key=$API_KEY")
-    fun getCryptoCurrencies(): Call<List<Coin>>
+    @GET("mktcapfull?tsym=EUR")
+    fun getCryptoCurrencies(@Query("page") page_no: Int): Call<CoinResponse>
 }
