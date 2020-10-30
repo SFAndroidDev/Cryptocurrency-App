99466886a37e51818ad1ef0dc67d1f562b48bde3
SF <sfnaiech2016@gmail.com>
2020-10-30 10:01:12 +0100

    detailsActivity implemented

--- app/src/main/java/com/sfprojects/android/cryptocurrencyapp/view/activities/DetailsActivity.kt
+++ app/src/main/java/com/sfprojects/android/cryptocurrencyapp/view/activities/DetailsActivity.kt
@@ -2,11 +2,21 @@ package com.sfprojects.android.cryptocurrencyapp.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.gson.Gson
 import com.sfprojects.android.cryptocurrencyapp.R
+import com.sfprojects.android.cryptocurrencyapp.network.model.Datum
+import kotlinx.android.synthetic.main.activity_details.*
 
 class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+
+        val data = intent.getSerializableExtra("DATA_PASS_") as? Datum
+
+        Log.d("DETAILS_TAG", "onCreate: ")
+
+        tv_details.text = Gson().toJson(data!!.display.eur)
     }
 }
