81c54375e09269694c02d568ab3c24792e87e567
SF <sfnaiech2016@gmail.com>
2020-11-04 17:08:35 +0100

    The service is calling the API every 5 seconds in the repository

--- app/src/main/java/com/sfprojects/android/cryptocurrencyapp/repository/CurrenciesActivityRepository.kt
+++ app/src/main/java/com/sfprojects/android/cryptocurrencyapp/repository/CurrenciesActivityRepository.kt
@@ -5,14 +5,18 @@ import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import com.google.gson.Gson
+import com.sfprojects.android.cryptocurrencyapp.network.model.CoinResponse
 import com.sfprojects.android.cryptocurrencyapp.network.retrofit.BASE_URL
 import com.sfprojects.android.cryptocurrencyapp.network.retrofit.CurrenciesNetwork
-import com.sfprojects.android.cryptocurrencyapp.network.model.CoinResponse
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 
 /**
  * Created by SFDevelop on 10/26/20.
@@ -23,13 +27,18 @@ class CurrenciesActivityRepository(val application: Application) {
     val coinResponse = MutableLiveData<CoinResponse>()
     internal lateinit var service: CurrenciesNetwork
 
+    companion object {
+        const val PERIOD_OF_POLLING: Long = 5
+        //TODO change PERIOD_OF_POLLING to 120 seconds
+    }
+
     fun changeState(){
         showProgress.value = !(showProgress.value !=null && showProgress.value!!)
     }
 
     fun fetchTopCoins(page_no: Int){
         showProgress.value = true
-
+        var myCounter = 0
         // Network call
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
@@ -38,6 +47,23 @@ class CurrenciesActivityRepository(val application: Application) {
 
         service = retrofit.create(CurrenciesNetwork::class.java)
 
+        // First API call
+        fetchData(page_no)
+
+        // Next API calls
+        Observable.interval(PERIOD_OF_POLLING, TimeUnit.SECONDS, Schedulers.io())
+            .observeOn(Schedulers.newThread())
+            .map { fetchData(page_no) }
+            .doOnError { error -> Log.i("ERROR_TAG", error.toString()) }
+            .retry()
+            .subscribe { fetchData(page_no)
+                Log.i("MY_COUNTER_TAG", "myCounter: " + myCounter++)
+            }
+
+
+    }
+
+    private fun fetchData(page_no: Int) =
         service.getCryptoCurrencies(page_no).enqueue(object : Callback<CoinResponse> {
             override fun onResponse(
                 call: Call<CoinResponse>,
@@ -48,7 +74,8 @@ class CurrenciesActivityRepository(val application: Application) {
                         Log.d("RESPONSE_TAG", " Response : ${Gson().toJson(response.body())}")
                         coinResponse.value = response.body();
                     } else {
-                        Toast.makeText(application, "UnExpected Response", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(application, "UnExpected Response", Toast.LENGTH_SHORT)
+                            .show()
                     }
                 } else {
                     Toast.makeText(application, response.message(), Toast.LENGTH_SHORT).show()
@@ -62,7 +89,7 @@ class CurrenciesActivityRepository(val application: Application) {
             }
         })
 
-    }
+
 
 
 }
